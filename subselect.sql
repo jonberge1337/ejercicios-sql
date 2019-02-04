7e475e2342d2b8895c0a4435cc840a3c4e49503b
jonberge1337 <jonbergerandi@gmail.com>
2019-02-04 13:45:40 +0100

    copiado el enunciado

---
+++ subselect.sql
@@ -0,0 +1,53 @@
+/* 21. Seleccionar los artículos cuyas existencias en almacén sean de menor cantidad, entre los que tienen existencias (no nulo). */
+
+
+/* 22. Seleccionar los artículos cuyo precio de costo sea inferior al precio de todos los artículos vendidos. */
+
+
+/* 23. Seleccionar los artículos que más beneficios nos proporcione su venta, así como dicho beneficio. */
+
+
+/* 24. Obtener los clientes que posean los albaranes con fecha de pago más reciente. */
+
+
+/* 25. Obtener los proveedores cuyos artículos tienen el mayor margen de existencias en el almacén (existencias – bajo_mínimo), dentro de los que pertenecen a las provincias que terminan con la letra D. */
+
+
+/* 26. Seleccionar los proveedores que pertenezcan a alguna provincia que empiece por la letra G que a su vez contenga algún cliente con forma de pago al contado. */
+
+
+/* 27. Obtener las provincias que tienen clientes y proveedores. */
+
+
+/* 28. Presentar la lista de clientes que han realizado alguna compra que no pertenecen a ninguna provincia que empiece por ‘M’. */
+
+
+/* 29. Mostrar los clientes que no hayan pagado al contado sus pedidos en el mes de marzo de 1988. */
+
+
+/* 30. Mostrar los clientes que tengan alguna compra con forma de pago diferente a la que tienen asignada por defecto. */
+
+ 
+
+/* 31. Mostrar los albaranes que no posean líneas. */
+
+
+/* 32. Seleccionar los clientes que contengan los albaranes en el 1er trimestre de 1988. */
+
+
+/* 33. Seleccionar las provincias que no posean clientes. */
+
+
+/* 34. Seleccionar las provincias que posean clientes sin albaranes. */
+
+
+/* 35. Obtener las provincias que contienen clientes y proveedores. */
+
+
+/* 36. Obtener las provincias con clientes y con proveedores que nos suministren algún artículo. */
+
+
+
+/* 37. Obtener las provincias que contengan todas las formas de pago entre todos sus clientes. */
+
+/*  38. Seleccionar los proveedores que tengan entre sus articulos todas las unidades de medida. */
