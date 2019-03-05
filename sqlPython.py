9b663faf2c62ba7ee5b45e48f6667c813a5dfbd8
jonberge1337 <jonbergerandi@gmail.com>
2019-03-05 17:38:29 +0100

    añadido altas bajas y consultas

--- sqlPython.py
+++ sqlPython.py
@@ -1,6 +1,19 @@
+import cx_Oracle
+
+def leerSql(archivo):
+
+    with open(archivo) as fichero:
+        return fichero
+
+
 f = open('tabledefinition.sql')
 full_sql = f.read()
 sql_commands = full_sql.split(';')
 
 for sql_command in sql_commands:
     curs.execute(sql_command)
+
+
+with open("subselect.sql") as f:
+    for i in f:
+        print(i)
